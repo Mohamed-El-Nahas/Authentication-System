3b710fae8afb2bea50252f8024a4dbbe68e92efa
Mohamed-El-Nahas <135478118+Mohamed-El-Nahas@users.noreply.github.com>
2025-02-02 07:50:30 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,28 @@ const allowedOrigins = [
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // API END-POINTS
 app.get('/', (req, res) => res.send("SERVER RUNING AT PORT - 5000"));
